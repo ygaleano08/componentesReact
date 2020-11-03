b981217541a547aedcb675b1ae4caf11b129a017
ygaleano08 <65578250+ygaleano08@users.noreply.github.com>
2020-11-03 17:30:21 -0600

    changes xy

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,7 +4,7 @@ export default function Icon(props) {
   return (
     <div className="icon">
       <img src={props.icon} alt="icon" />
-      <h3 className="title2 xy">{props.title}</h3>
+      <h3 className="xy">{props.title}</h3>
     </div>
   );
 }
